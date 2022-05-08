868d0a554424b5b8bf1e52a2316b513687c043da
HIKARI\Hikari <kmuu011@naver.com>
2022-05-08 17:00:03 +0900

    feat 권한 체크용 API 개발 및 guard 완성 로그인 기능 완성 및 로그인 정보 저장하게 구현

--- nestjs/src/member/member.controller.ts
+++ nestjs/src/member/member.controller.ts
@@ -1,28 +1,67 @@
-import {Body, Controller, Get, Post, Req} from '@nestjs/common';
+import {Body, Controller, Get, Post, Req, UseGuards} from '@nestjs/common';
+import {Request} from "express";
+
 import {MemberService} from './member.service';
 
 import {LoginMemberDto} from "./dto/login-member.dto";
 import {CreateMemberDto} from "./dto/create-member-dto";
 import {DuplicateCheckMemberDto} from "./dto/duplicate-check-member.dto";
-import {Message} from "libs/message";
+import {Member} from "./entities/member.entity"
 
-const duplicateCheckKeys = ['id', 'nickname', 'email'];
+import {AuthGuard} from "guard/auth.guard";
 
 @Controller('/member')
 export class MemberController {
     constructor(
         private readonly memberService: MemberService,
-    ) {}
+    ) {
+    }
+
+    @Post('/auth')
+    @UseGuards(AuthGuard)
+    async auth(
+        @Req() req: Request
+    ) {
+        const memberInfo = req.body.memberInfo;
+
+        return {
+            memberInfo
+        };
+    }
+
+    @Post('/login')
+    async login(
+        @Req() req: Request,
+        @Body() loginMemberDto: LoginMemberDto
+    ) {
+        const member: Member = await this.memberService.login(loginMemberDto, req.headers);
+
+        return {
+            token: member.token
+        };
+    }
+
 
     @Post('/signUp')
     async signUp(
         @Body() createMemberDto: CreateMemberDto
-    ){
-        const signUpResult = await this.memberService.signUp(createMemberDto);
+    ) {
+        await this.memberService.signUp(createMemberDto);
 
-        return true;
+        return {
+            result: true
+        };
     }
 
+
+    @Get('/duplicateCheck')
+    async duplicateCheck(
+        @Body() duplicateCheckDto: DuplicateCheckMemberDto
+    ) {
+        return {
+            result: await this.memberService.duplicateCheck(duplicateCheckDto)
+        };
+    }
 
 
 }
