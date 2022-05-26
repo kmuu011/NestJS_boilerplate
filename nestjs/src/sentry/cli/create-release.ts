442f152650c1a8404076a29a0a7b3c64646a7781
HIKARI\Hikari <kmuu011@naver.com>
2022-05-26 22:04:28 +0900

    feat: 서버 컨트롤러 업데이트 및 sentry cli의 command실행시 auth정보 필요없게 자동화

--- nestjs/src/sentry/cli/create-release.ts
+++ nestjs/src/sentry/cli/create-release.ts
@@ -9,10 +9,10 @@ export const sentrySettingRun = async () => {
         // let version = await runCli('sentry-cli releases propose-version');
         // version = version.substring(0, version.indexOf('\n'));
 
-        await runCli(`sentry-cli releases new -p ${sentryConfig.project} ${releaseName} --org ${sentryConfig.org}`)
+        await runCli(`sentry-cli releases new -p ${sentryConfig.project} ${releaseName} --org ${sentryConfig.org} --auth-token ${sentryConfig.token}`)
 
-        await runCli(`sentry-cli releases --org ${sentryConfig.org} deploys ${releaseName} new -e ${environment} `);
-        await runCli(`sentry-cli releases --org ${sentryConfig.org} set-commits ${releaseName} --auto `);
+        await runCli(`sentry-cli releases --org ${sentryConfig.org} deploys ${releaseName} new -e ${environment} --auth-token ${sentryConfig.token}`);
+        await runCli(`sentry-cli releases --org ${sentryConfig.org} set-commits ${releaseName} --auto --auth-token ${sentryConfig.token}`);
 
     } catch (error) {
         console.log(error);
